2eeb5ae548f5d98a0c21a726cace480031475a4e
FHAO-3 <62599735+FHAO-3@users.noreply.github.com>
2022-11-07 20:35:56 -0300

    improving the js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,112 +1,93 @@
 function sendStringToPage(value) {
     const countainer = document.querySelector('.container');
     countainer.innerHTML = `<h1>${value}</h1>`;
-    
+
 };
 
 function addZero(number) {
     // adiciona o 0 (Zero) caso sejaum numero menor que 10
-    return number < 10 ? `0${number}`: number;
+    return number < 10 ? `0${number}` : number;
 };
 
-function line(params) {
+function line() {
     console.log('--------------------------------------------------------------------');
-}
+};
+
+function strMonth(params) {
+    switch (month) {
+        case 0:
+            return 'Janeiro';
+        case 1:
+            return 'Fevereiro';
+        case 2:
+            return 'Março';
+        case 3:
+            return 'Abril';
+        case 4:
+            return 'Maio';
+        case 5:
+            return 'Junho';
+        case 6:
+            return 'Julho';
+        case 7:
+            return 'Agosto';
+        case 8:
+            return 'Setembro';
+        case 9:
+            return 'Outubro';
+        case 10:
+            return 'Novembro';
+        case 11:
+            return 'Dezembro';
+        default:
+            return 'ERRO: opção inexistente!'
+    };
+};
+
+function strDayWeek() {
+    const dayWeek = date.getDay();
+    switch (dayWeek) {
+        case 0:
+            return `Domingo`;
+        case 1:
+            return `Segunda-Feira`;
+        case 2:
+            return `Terça-Feira`;
+        case 3:
+            return `Quarta-Feira`;
+        case 4:
+            return `Quinta-Feira`;
+        case 5:
+            return `Sexta-Feira`;
+        case 6:
+            return `Sabado`;
+        default:
+            return 'ERRO: Esse valor é invalido';
+    };
+};
+
+function partDay(value) {
+    if (value < 5) {
+        return 'Boa madrugada!';
+    } else if (value < 12) {
+        return 'Bom dia!';
+    } else if (value < 17) {
+        return 'Boa tarde!';
+    } else if (value < 23) {
+        return 'Boa noite!'
+    };
+};
 
 const date = new Date();
 const day = addZero(date.getDate());
+const month = date.getMonth();
 const year = date.getFullYear();
 const hour = addZero(date.getHours());
 const minute = addZero(date.getMinutes())
 const seconds = addZero(date.getSeconds());
 const milleseconds = date.getMilliseconds();
+const pDay = partDay(hour);
 
-const month = date.getMonth();
-let strMonth;
-switch (month) {
-    case 0:
-        strMonth ='Janeiro'
-        break;
-    case 1:
-        strMonth = 'Fevereiro'
-        break;
-    case 2:
-        strMonth = 'Março'
-        break;
-    case 3:
-        strMonth = 'Abril'
-        break;
-    case 4:
-        strMonth = 'Maio'
-        break;
-    case 5:
-        strMonth = 'Junho'
-        break;
-    case 6:
-        strMonth = 'Julho'
-        break;
-    case 7:
-        strMonth = 'Agosto'
-        break;
-    case 8:
-        strMonth = 'Setembro'
-        break;
-    case 9:
-        strMonth = 'Outubro'
-        break;
-    case 10:
-        strMonth = 'Novembro'
-        break;
-    case 11:
-        strMonth = 'Dezembro'
-        break;
-    default:
-        break;
-};
+const txt = `${pDay} <br><br>Data: ${strDayWeek()} ${day} /${strMonth()}/${year}<br><br>Hora: ${hour}:${minute}:${seconds}.${milleseconds}`
 
-const dayWeek = date.getDay();
-let strDayWeek;
-switch (dayWeek) {
-    case 0:
-        strDayWeek = `Domingo`;
-        break;
-    case 1:
-        strDayWeek = `Segunda-Feira`;
-        break;
-    case 2:
-        strDayWeek = `Terça-Feira`;
-        break;
-    case 3:
-        strDayWeek = `Quarta-Feira`;
-        break;
-    case 4:
-        strDayWeek = `Quinta-Feira`;
-        break;
-    case 5:
-        strDayWeek = `Sexta-Feira`;
-        break;
-    case 6:
-        strDayWeek = `Sabado`;
-        break;
-
-    default:
-        strDayWeek = 'ERRO: Esse valor é invalido';
-        break;
-};
-
-let txtFinal;
-
-if (hour < 23 && hour > 18){
-    txtFinal = `Boa Noite! <br> ${strDayWeek} ${day} /${strMonth}/${year} ${hour}:${minute}:${seconds}.${milleseconds}`
-} else if (hour < 18 && hour > 12){
-    txtFinal = `Boa Tarde! <br> ${strDayWeek} ${day} /${strMonth}/${year} ${hour}:${minute}:${seconds}.${milleseconds}`
-} else if (hour < 12 && hour > 5) {
-    txtFinal = `Bom Dia! <br> ${strDayWeek} ${day} /${strMonth}/${year} ${hour}:${minute}:${seconds}.${milleseconds}`
-} else if (hour < 5 && hour > 0) {
-    txtFinal = `Boa Madrugada! <br> ${strDayWeek} ${day} /${strMonth}/${year} ${hour}:${minute}:${seconds}.${milleseconds}`
-}
-line()
-console.log(txtFinal);
-line()
-
-sendStringToPage(txtFinal)
+sendStringToPage(txt);
